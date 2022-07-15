94b4f319efe6f1a6ffa8233e4beeb5f6203e9ef2
sheryak_n <nathanraysheryak@gmail.com>
2022-07-15 07:33:36 -0400

    scrap old project

--- playground/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def say_hello_html(request):
-  return HttpResponse('<h1>allo html!</h1>')
-
-def say_hello(request):
-  return render(request, 'hello.html', { 'name': 'Tsuki' })
