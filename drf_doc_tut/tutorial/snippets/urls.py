7d24c42fbcedef70a1c89d4b19ae7d9f249a0ab8
sheryak_n <nathanraysheryak@gmail.com>
2022-07-24 08:23:46 -0400

    drf part 3.1 class based views

--- drf_doc_tut/tutorial/snippets/urls.py
+++ drf_doc_tut/tutorial/snippets/urls.py
@@ -4,8 +4,12 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
+    # Url patterns for FUNCTION based views
     path("snippets/", views.snippet_list),
     path("snippets/<int:pk>/", views.snippet_detail),
+    # Url patterns for CLASS based views
+    path("snippets/", views.SnippetList.as_view()),
+    path("snippets/<int:pk>/", views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
