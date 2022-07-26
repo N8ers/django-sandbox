0809a5083286b3501973f28ce25e6879832b5e6d
sheryak_n <nathanraysheryak@gmail.com>
2022-07-26 06:44:06 -0400

    refactor logs/views to use @api_view

--- captains-log/captains_log/logs/urls.py
+++ captains-log/captains_log/logs/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from logs import views
 
 urlpatterns = [
     path("logs/", views.log_list),
     path("logs/<int:pk>/", views.log_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- captains-log/captains_log/logs/views.py
+++ captains-log/captains_log/logs/views.py
@@ -1,46 +1,53 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from cmath import log10
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
 
 from logs.models import Log
 from logs.serializers import LogSerializer
 
+# 2 ways of writing API views
+# 1. @api_view decorator for function based views
+# 2. APIView class for class based views
 
-@csrf_exempt
+
+@api_view(["GET", "POST"])
 def log_list(request):
     if request.method == "GET":
         logs = Log.objects.all()
         serializer = LogSerializer(logs, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == "POST":
         data = JSONParser().parse(request)
         serializer = LogSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(["GET", "PUT", "DELETE"])
 def log_detail(request, pk):
+
     try:
-        snippet = Log.objects.get(pk=pk)
+        log = Log.objects.get(pk=pk)
     except Log.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == "GET":
-        serializer = LogSerializer(snippet)
-        return JsonResponse(serializer.data)
+        serializer = LogSerializer(log)
+        return Response(serializer.data)
 
     elif request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = LogSerializer(snippet, data=data)
+        serializer = LogSerializer(log, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == "DELETE":
-        snippet.delete()
-        return HttpResponse(status=204)
+        log10.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
